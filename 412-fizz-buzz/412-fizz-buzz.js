0a01ed596d4039f79beea8310bd956e33968822f
Dagmawi Gezachew <zdagmawig1@gmail.com>
2022-08-19 21:35:11 +0300

    Time: 136 ms (6.68%), Space: 44.4 MB (53.48%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.js
+++ 412-fizz-buzz/412-fizz-buzz.js
@@ -3,8 +3,8 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-let arr=[]
-  for(let i=1, s=''; i<n+1; i++){
+let arr=[],s=""
+  for(let i=1; i<n+1; i++){
     s = (i%3?'':'Fizz')+(i%5?'':'Buzz')||i;
     arr.push(""+s);
   }
