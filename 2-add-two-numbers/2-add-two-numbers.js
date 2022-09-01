b0470b87c18acc010c15687850890c0b9bf5bd2b
Dagmawi Gezachew <zdagmawig1@gmail.com>
2022-09-01 21:43:26 +0300

    Time: 213 ms (15.22%), Space: 46.7 MB (93.75%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    var result = new ListNode(0);
+    var current = result;
+    var carry = 0;
+    while (l1 || l2 || carry) {
+        var sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+    return result.next;
+};
