81b86248e14ef2f0650242d5559866686efbd2ea
Dagmawi Gezachew <zdagmawig1@gmail.com>
2022-09-11 17:46:28 +0300

    Time: 285 ms (5.01%), Space: 48.9 MB (85.80%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    for(let i=0;i<s.length;i++){
+       s.splice(i, 0, s.pop())
+    }
+    return s
+};
