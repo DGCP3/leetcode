4a3828db222396231c69c71940ed310768e6c338
Dagmawi Gezachew <zdagmawig1@gmail.com>
2022-11-07 22:18:54 +0300

    Time: 121 ms (24.01%), Space: 43.7 MB (56.06%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function(nums, target) {
+    nums=nums.sort((a,b)=>a-b)
+    const arr=[]
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===target){
+            arr.push(i)
+        }
+    }
+    return arr
+};
